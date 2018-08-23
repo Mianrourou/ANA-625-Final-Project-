15a0595a036cf24d25b93c81cb891831e2854cf9
Chengzhi Li <lcz9188@yahoo.com>
2018-08-22 21:58:17 -0700

    uploade

---
+++ Final Project.R
@@ -0,0 +1,36 @@
+mydata=CableTVSubscribersData
+#MDA PART
+library(psych)
+#scater plot matrix for general data set
+pairs.panels (CableTVSubscribersData[c("age", "gender", "income", "kids","subscribe","Segment")])
+#box plot for Income and Subscribe
+boxplot(CableTVSubscribersData$income~CableTVSubscribersData$subscribe,main="Boxplot for Income and Subscribe",col="red",ylab="Income($)",xlab="Subscribtion")
+#box plot for age and subscribe
+boxplot(CableTVSubscribersData$age~CableTVSubscribersData$subscribe,main="Boxplot for Age and Subscribe",col="red",ylab="Age(Years)",xlab="Subscribtion")
+#QQline for Income
+qqnorm(CableTVSubscribersData$income,main = "quantile-quantile plots for Income")
+#QQline for Age
+qqnorm(CableTVSubscribersData$age,main = "quantile-quantile plots for Age")
+#Density for income
+plot(density(CableTVSubscribersData$income),main = "Density Plot for Income")
+#checking categorical data if as factor
+str(CableTVSubscribersData)
+#having a overview of the data
+summary(CableTVSubscribersData)
+#checking class of variables
+class(CableTVSubscribersData$age)
+class(CableTVSubscribersData$Segment)
+class(CableTVSubscribersData$subscribe)
+class(CableTVSubscribersData$income)
+class(CableTVSubscribersData$kids)
+class(CableTVSubscribersData$ownHome)
+#checking missing values
+is.na(CableTVSubscribersData)
+#making variable as factor if R does not recongnize it as factor
+as.factor(CableTVSubscribersData$gender)
+
+#delet entire row of missing values(however, we dont have any missing values for this data)
+# ----na.omit()----
+#logistic regression
+glm(subscribe~age+gender+income+kids+ownHome+Segment,data=CableTVSubscribersData,family=binomial)
+#review chapter 2 for EDA parts
